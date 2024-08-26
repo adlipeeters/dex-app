e30791cc775d1d51666c0d6f9baab574196e12bb
adlipeeters <adlipeeters@gmail.com>
2024-08-26 22:51:12 +0300

    commit

--- constants/qite-dex-constants.ts
+++ constants/qite-dex-constants.ts
@@ -4,18 +4,18 @@ import qitePoolAbi from './abi/qite-pool-abi.json';
 import qitePoolTokenAbi from './abi/qite-pool-token-abi.json';
 
 
-// /token1_address = "0x674cE055435699a6Cea3Dc500847669663f2d1DC"
-// /token1_name = "TEST1"
-// /token2_address = "0xFe2f4470bd6fE00e6804fC76087c363e5bb400B1"
-// /token2_name = "TEST2"
+// /token1_address = "0x56A0A8524F71c2947434172abFBAF80c1aC6D70e"
+// /token1_name = "TOKEN1"
+// /token2_address = "0x5A12796B21B3c275e47ECBb4E39F9a114239913a"
+// /token2_name = "TOKEN2"
 
 const qiteAddresses: QiteAddresses = {
     "11155111": {
-        "qiteSwap": "0x79DfCeb440AFb6B3C5389b4833F7334a194E0A7c"
+        "qiteSwap": "0xed3a65f32b2f81E95E9eDEB87D792278D381f876"
     }
 }
 
-const adminAddress = "0x4be5b8Ef34d844B04260eA72341169546bE0E288";
+const adminAddress = "fakeAdminAddress";
 
 export {
     qiteDexAbi,
